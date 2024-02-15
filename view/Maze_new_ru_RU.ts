902f015158c9d47e085fdf21e4b0b7bd9c8d0432
Jetta Jacks <jettajac@at-b1.msk.21-school.ru>
2024-02-15 19:08:47 +0300

    project - Done, next will add images result

---
+++ view/Maze_new_ru_RU.ts
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="26"/>
+        <location filename="Maze_new_autogen/include/ui_mainwindow.h" line="256"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="61"/>
+        <location filename="Maze_new_autogen/include/ui_mainwindow.h" line="257"/>
+        <source>Generate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="84"/>
+        <location filename="Maze_new_autogen/include/ui_mainwindow.h" line="258"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="107"/>
+        <location filename="Maze_new_autogen/include/ui_mainwindow.h" line="259"/>
+        <source>Open file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="130"/>
+        <location filename="Maze_new_autogen/include/ui_mainwindow.h" line="260"/>
+        <source>Off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="174"/>
+        <location filename="Maze_new_autogen/include/ui_mainwindow.h" line="261"/>
+        <source>Points to find way</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="256"/>
+        <location filename="Maze_new_autogen/include/ui_mainwindow.h" line="262"/>
+        <source>y</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="307"/>
+        <location filename="Maze_new_autogen/include/ui_mainwindow.h" line="263"/>
+        <source>X</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="317"/>
+        <location filename="Maze_new_autogen/include/ui_mainwindow.h" line="264"/>
+        <source>Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
